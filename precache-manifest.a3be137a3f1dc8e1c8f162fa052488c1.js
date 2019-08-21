8bba4cafbcbc959279a4753972c7afa81019f4fe
jorgevels <jorgevelasquez006@hotmail.com>
2019-08-21 07:17:32 -0500

    Updates

---
+++ precache-manifest.a3be137a3f1dc8e1c8f162fa052488c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dcdd59f5b02497f2054e6de32ed961c",
+    "url": "/static/media/perfilP.7dcdd59f.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4dcf5c861ed733aaa634",
+    "url": "/static/js/main.4dcf5c86.chunk.js"
+  },
+  {
+    "revision": "7f6d1ecf21195213c0a3",
+    "url": "/static/js/1.7f6d1ecf.chunk.js"
+  },
+  {
+    "revision": "4dcf5c861ed733aaa634",
+    "url": "/static/css/main.7334e5eb.chunk.css"
+  },
+  {
+    "revision": "7f6d1ecf21195213c0a3",
+    "url": "/static/css/1.6d96f327.chunk.css"
+  },
+  {
+    "revision": "af6ef05f9a461945980b64f10090b092",
+    "url": "/index.html"
+  }
+];
